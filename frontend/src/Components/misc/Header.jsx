489162342f199fa4767593b2b9c26127b04d5866
pratham khandelwal <prathamkhandelwal@prathams-MacBook-Air.local>
2025-03-07 22:46:39 +0530

    profile pic change

--- frontend/src/Components/misc/Header.jsx
+++ frontend/src/Components/misc/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
 
   return <header className=" relative p-5 border-b-2 border-black flex items-center">
     <div className=" absolute w-[30%] right-0 flex justify-end items-center mr-10 ">
-      <img src="https://ui-avatars.com/api/?name=pk&background=000000&color=ffffff&bold=true" className="rounded-full h-10" alt="" />
+      <img src="https://ui-avatars.com/api/?name=vk&background=000000&color=ffffff&bold=true" className="rounded-full h-10" alt="" />
       <IoExitOutline size={40}  className="ml-6" onClick={handleClick}/>
     </div>
   </header>;
